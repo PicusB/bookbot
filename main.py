70888b8d439c70ae0c09f9ea4655bbe5ef242c0c
PicusB <picusbrit@gmail.com>
2025-07-04 18:28:39 -0400

    modified to accept any path to a text file for a book - Final stage of project reqs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words
 from stats import count_characters
 from stats import sort_character_dictionary
@@ -9,10 +10,14 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = sys.argv[1]
+    book_text = get_book_text(path_to_book)
     book_word_count = count_words(book_text)
     print(f"{book_word_count} words found in the document")
     letter_dictionary = count_characters(book_text)
-    printreport(book_word_count, sort_character_dictionary(letter_dictionary), "books/frankenstein.txt")
+    printreport(book_word_count, sort_character_dictionary(letter_dictionary), path_to_book)
 
 main()
